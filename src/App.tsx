22d8c8fecfea226d9eeb7476346ea3d6fd9319fc
Mustafa <218740897+0x99M@users.noreply.github.com>
2025-08-07 09:51:18 +0300

    Implement Tabs to show albums, artists, and songs

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
 import './App.css'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
 
 function App() {
   return (
-    <></>
+    <div className='h-screen w-full py-12 flex flex-col items-center justify-start'>
+      <Tabs defaultValue='albums'>
+        <TabsList>
+          <TabsTrigger value='albums'>Albums</TabsTrigger>
+          <TabsTrigger value='songs'>Songs</TabsTrigger>
+          <TabsTrigger value='artists'>Artists</TabsTrigger>
+        </TabsList>
+        <TabsContent value='albums'>Albums Data.</TabsContent>
+        <TabsContent value='songs'>Songs Data.</TabsContent>
+        <TabsContent value='artists'>Artists Data.</TabsContent>
+      </Tabs>
+    </div>
   )
 }
 
