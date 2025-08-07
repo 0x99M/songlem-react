46820db4919872765559be8d364af112bbd5bc29
Mustafa <218740897+0x99M@users.noreply.github.com>
2025-08-07 13:08:28 +0300

    Add sleeping backend service warning

--- src/App.tsx
+++ src/App.tsx
@@ -70,7 +70,10 @@ function App() {
   );
 
   return (
-    <div className='h-fit w-full px-12 py-12 flex flex-col items-center justify-start'>
+    <div className='h-fit w-full px-12 py-12 flex flex-col items-center justify-start gap-8'>
+      <div className='px-4 py-2 rounded-xl bg-neutral-800 text-neutral-100' >
+        <h3>The backend service might be sleeping. Wait 60 seconds it will wake up!</h3>
+      </div>
       <Tabs defaultValue='albums' className='w-fit text-center flex flex-col items-center justify-center' onValueChange={handleTabChange}>
         <TabsList>
           <TabsTrigger value='albums'>Albums</TabsTrigger>
